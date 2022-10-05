d309e25e9560f5deea9e4bb9805461c038b9689f
Paul Schilling <mail@paulschilling.de>
2022-10-05 21:16:58 +0200

    simplify setup for Neovim     - symlink directly to ~/.config/nvim/init.vim

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ ln -sf ~/.dotfiles/.bash_profile ~
 ln -sf ~/.dotfiles/.bashrc ~
 ln -sf ~/.dotfiles/.gitconfig ~
 ln -sf ~/.dotfiles/.gitignore ~
-ln -sf ~/.dotfiles/.vimrc ~
+ln -sf ~/.dotfiles/.vimrc ~/.config/nvim/init.vim
 ln -sf ~/.dotfiles/.pylintrc ~
 
 # Copy folders
@@ -14,10 +14,6 @@ cp -r ~/.dotfiles/.vim_templates ~
 # Load terminal settings
 cat ~/.dotfiles/gnome-terminal.preferences | dconf load /org/gnome/terminal/
 
-# Setup Neovim
-# enable Plug
+# Enable Plug for Neovim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-# make Neovim use .vimrc
-mkdir -p ~/.config/nvim; ln -s ~/.vimrc ~/.config/nvim/init.vim
