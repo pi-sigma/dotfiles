e1097016d2fa07f8eca8ba60ff9625335f2b9b6f
Paul Schilling <mail@paulschilling.de>
2022-11-22 08:28:40 +0100

    fix LSP keymaps

--- nvim/config.lua
+++ nvim/config.lua
@@ -33,10 +33,10 @@ local on_attach = function(client, bufnr)
 
 	local bufopts = { noremap=true, silent=true, buffer=bufnr }
 	vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-	vim.keymap.set('n', 'ggd', vim.lsp.buf.definition, bufopts)
+	vim.keymap.set('n', '<space>gd', vim.lsp.buf.definition, bufopts)
 	vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
 	vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-	vim.keymap.set('n', 'ggD', vim.lsp.buf.declaration, bufopts)
+	vim.keymap.set('n', '<space>gD', vim.lsp.buf.declaration, bufopts)
 	-- vim.keymap.set('n', '<space>K', vim.lsp.buf.signature_help, bufopts)
 	-- vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, bufopts)
 	vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, bufopts)
