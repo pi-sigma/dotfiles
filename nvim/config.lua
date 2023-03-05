6a073bef3666985a53666d2c5e583e22d048acbd
Paul Schilling <mail@paulschilling.de>
2023-03-05 10:42:14 +0100

    nvim: plugins, clean up
    *add plugin for handling buffers
    *clean up vimrc and config.lua

--- nvim/config.lua
+++ nvim/config.lua
@@ -4,18 +4,19 @@ local cmp = require('cmp')
 lsp.preset('recommended')
 lsp.setup()
 
+
 -- Diagnostics
 vim.diagnostic.config({
   severity_sort = true,
   float = {
-    source = "always",  -- Or "if_many"
+    source = "always",
   },
 })
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
 	vim.lsp.diagnostic.on_publish_diagnostics, {
 		signs = true,
 		underline = false,
-		virtual_text = false,
+		virtual_text = true,
 		update_in_insert = false,
 	}
 )
@@ -26,26 +27,32 @@ for type, icon in pairs(signs) do
     vim.fn.sign_define(hl, { text = icon, texthl= hl, numhl = hl })
 end
 
-
 local on_attach = function(client, bufnr)
 	-- Enable completion triggered by <c-x><c-o>
 	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
 	local bufopts = { noremap=true, silent=true, buffer=bufnr }
 	vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-	-- vim.keymap.set('n', '<space>gd', vim.lsp.buf.definition, bufopts)
 	vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
 	vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-	vim.keymap.set('n', '<space>gD', vim.lsp.buf.declaration, bufopts)
 	-- vim.keymap.set('n', '<space>K', vim.lsp.buf.signature_help, bufopts)
 	-- vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, bufopts)
 	vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, bufopts)
-	-- vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-	-- vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
 	vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-	vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-	vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 end
+
+-- Global completion setup
+cmp.setup {
+	window = {
+		-- don't show documentation window on complete
+		documentation = {
+			max_width = 0
+		}
+	},
+	completion = {
+		autocomplete = false
+	}
+}
 
 -- C
 require('lspconfig')['clangd'].setup {
@@ -53,22 +60,34 @@ require('lspconfig')['clangd'].setup {
 	flags = {
 		debounce_text_changes = 150
 	},
-	cmp.setup {
-		completion = {
-			autocomplete = false
+}
+-- Lua
+require('lspconfig')['lua_ls'].setup {
+	on_attach = on_attach,
+	flags = {
+		debounce_text_changes = 150
+	},
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { 'vim', 'opts' }
+			}
 		}
 	}
+}
+-- JavaScript/TypeScript
+require('lspconfig')['tsserver'].setup {
+	on_attach = on_attach,
+	flags = {
+		debounce_text_changes = 150
+	},
+	cmd = { 'typescript-language-server', '--stdio' },
 }
 -- Python
 require('lspconfig')['pylsp'].setup {
 	on_attach = on_attach,
 	flags = {
 		debounce_text_changes = 150
-	},
-	cmp.setup {
-		completion = {
-			autocomplete = false
-		}
 	},
 	settings = {
 		pylsp = {
@@ -89,58 +108,17 @@ require('lspconfig')['pylsp'].setup {
 		}
 	}
 }
--- Lua
-require('lspconfig')['sumneko_lua'].setup {
-	on_attach = on_attach,
-	flags = {
-		debounce_text_changes = 150
-	},
-	cmp.setup {
-		completion = {
-			autocomplete = false
-		}
-	},
-	settings = {
-		Lua = {
-			diagnostics = {
-				globals = { 'vim', 'opts' }
-			}
-		}
-	}
-}
--- JavaScript/TypeScript
-require('lspconfig')['tsserver'].setup {
-	on_attach = on_attach,
-	flags = {
-		debounce_text_changes = 150
-	},
-	cmd = { 'typescript-language-server', '--stdio' },
-	cmp.setup {
-		completion = {
-			autocomplete = false
-		}
-	},
-}
 
 
 -- treesitter
 require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all" (the four listed parsers should always be installed)
   ensure_installed = { "c", "lua", "vim", "help" },
-
-  -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
-
-  -- Automatically install missing parsers when entering buffer
   auto_install = true,
-
-  -- List of parsers to ignore installing (for "all")
   ignore_install = { },
 
   highlight = {
-    -- `false` will disable the whole extension
-    enable = true,
-
+    enable = false,
     -- Use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
     disable = function(lang, buf)
         local max_filesize = 100 * 1024 -- 100 KB
