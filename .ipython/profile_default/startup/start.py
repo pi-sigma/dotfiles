4827e7c2ef4c48a4090c6ad26515a42a578e38f1
Paul Schilling <mail@paulschilling.de>
2024-01-23 21:39:17 +0100

    (ipython) Add autoreload to startup script

--- .ipython/profile_default/startup/start.py
+++ .ipython/profile_default/startup/start.py
@@ -1,9 +1,8 @@
-# imports
-from collections import defaultdict, namedtuple
-from decimal import Decimal
-from datetime import date, datetime, timezone
-import json
-from json import JSONDecoder, JSONEncoder
-import os, sys
-import re
-import zoneinfo
+import IPython
+
+ipython = IPython.get_ipython()
+
+ipython.run_line_magic(magic_name="load_ext", line="autoreload")
+ipython.run_line_magic(magic_name="autoreload", line="2")
+
+print("Autoreload enabled")
